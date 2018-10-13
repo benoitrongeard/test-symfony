8c8f787b01106d0d32213170f367c4c79dfb0d04
Benoit Rongeard <benron@hotmail.fr>
2018-10-13 18:40:32 +0200

    Clean appBundle + Add first controller/Wiew

---
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+    public function indexAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('name' => 'Benoit'));
+
+        return new Response($content);
+    }
+}
---
+++ src/OC/PlatformBundle/Tests/Controller/AdvertControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OC\PlatformBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdvertControllerTest extends WebTestCase
+{
+}
