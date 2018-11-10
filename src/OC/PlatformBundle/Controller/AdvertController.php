8d154420c8499896ece0669ce4d3b964549a1872
Benoit Rongeard <benron@hotmail.fr>
2018-11-10 12:43:47 +0100

    Add intities Image and Advert, Update AvertControllern and modify AdvertView

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,6 +4,8 @@
 
 namespace OC\PlatformBundle\Controller;
 
+use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -12,30 +14,12 @@ class AdvertController extends Controller
 {
   public function indexAction($page)
   {
-    $listAdverts = array(
-      array(
-        'title'   => 'Recherche développpeur Symfony',
-        'id'      => 1,
-        'author'  => 'Alexandre',
-        'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Mission de webmaster',
-        'id'      => 2,
-        'author'  => 'Hugo',
-        'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-        'date'    => new \Datetime()),
-      array(
-        'title'   => 'Offre de stage webdesigner',
-        'id'      => 3,
-        'author'  => 'Mathieu',
-        'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-        'date'    => new \Datetime())
-    );
+      $em = $this->getDoctrine()->getManager();
+      $listAdverts = $records = $em->getRepository("OCPlatformBundle:Advert")->findAll();
 
-    return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
-      'listAdverts' => $listAdverts
-    ));
+      return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+          'listAdverts' => $listAdverts
+      ));
   }
 
   public function menuAction($limit)
@@ -53,13 +37,10 @@ class AdvertController extends Controller
 
   public function viewAction($id)
   {
-    $advert = array(
-      'title'   => 'Recherche développpeur Symfony2',
-      'id'      => $id,
-      'author'  => 'Alexandre',
-      'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
-      'date'    => new \Datetime()
-    );
+      $em = $this->getDoctrine()->getManager();
+
+      // On récupère l'annonce
+      $advert = $em->getRepository('OCPlatformBundle:Advert')->find($id);
 
     return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
       'advert' => $advert
@@ -68,7 +49,30 @@ class AdvertController extends Controller
 
   public function addAction(Request $request)
   {
-    return $this->render('OCPlatformBundle:Advert:add.html.twig');
+      $advert = new Advert();
+      $advert->setTitle('Recherche développeur Symfony.');
+      $advert->setAuthor('Alexandre');
+      $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
+
+      $image = new Image();
+      $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+      $image->setAlt('Job de rêves');
+
+      $advert->setImage($image);
+
+      $em = $this->getDoctrine()->getManager();
+
+      $em->persist($advert);
+
+      $em->flush();
+
+      if ($request->isMethod('POST')) {
+          $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+          return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+      }
+
+      return $this->render('OCPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
   }
 
   public function editAction($id, Request $request)
